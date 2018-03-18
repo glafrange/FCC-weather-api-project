c5e0ad56fd2e3edebfa12e0d284788845fb834fb
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-03-18 00:38:07 +0000

    fixed location, added changeMeasurement function, changed colors

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,35 @@
 angular.module('weatherApiApp', [])
 
 .service('weatherService', function($http){
-  this.getWeather = function(callback){
-    $http.get('https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139')
+  
+  this.getWeather = function(lat, lon, callback){
+    var url = 'https://fcc-weather-api.glitch.me/api/current?lat='+lat+'&lon='+lon;
+    console.log(url);
+    $http.get(url)
     .then(callback);
   };
 })
 
 .controller('mainCtrl', function($scope, weatherService){
-  $scope.helloWorld = function(){
-    console.log($scope.coords);
-  };
-  weatherService.getWeather(function(response){
-    console.log(response.data);
-  });
+  
+  $scope.changeMeasurement = function(){
+    if($scope.temp.endsWith('C')){
+      $scope.temp = $scope.weatherData.main.temp * 2 + 30 + " F";
+    } else if ($scope.temp.endsWith('F')){
+      $scope.temp = $scope.weatherData.main.temp + " C"
+    }
+  }
   
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
       $scope.latitude = position.coords.latitude;
       $scope.longitude = position.coords.longitude;
+      weatherService.getWeather($scope.latitude, $scope.longitude, function(response){
+        $scope.weatherData = response.data;
+        $scope.temp = $scope.weatherData.main.temp + " C";
+        $scope.location = $scope.weatherData.name + ", " + $scope.weatherData.sys.country;
+      });
     });
   }
+  
 })
-
