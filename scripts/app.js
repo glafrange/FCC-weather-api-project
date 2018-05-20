d4d8df6cc55603d91b2f0cc09a82856bc7fbb8c8
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-19 23:13:25 -0400

    redone in jquery

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,47 @@
-angular.module('weatherApiApp', [])
-
-.service('weatherService', function($http){
+$(document).ready(function(){
   
-  this.getWeather = function(lat, lon, callback){
-    var url = 'https://fcc-weather-api.glitch.me/api/current?lat='+lat+'&lon='+lon;
-    console.log(url);
-    $http.get(url)
-    .then(callback);
-  };
-})
-
-.controller('mainCtrl', function($scope, weatherService){
+  var latitude, longitude, weatherData, temp, location, id;
   
-  
-  $scope.changeMeasurement = function(){
-    if($scope.temp.endsWith('C')){
-      $scope.temp = parseInt($scope.weatherData.main.temp) * 2 + 30 + String.fromCharCode(176) + "F";
-    } else if ($scope.temp.endsWith('F')){
-      $scope.temp = parseInt($scope.weatherData.main.temp) + String.fromCharCode(176) + "C"
+  function changeMeasurement(){
+    if(temp.endsWith('C')){
+      temp = parseInt(weatherData.main.temp) * 2 + 30 + String.fromCharCode(176) + "F";
+      $('#temp').html(temp);
+    } else if (temp.endsWith('F')){
+      temp = parseInt(weatherData.main.temp) + String.fromCharCode(176) + "C"
+      $('#temp').html(temp);
     }
+  }
+
+  $('#temp').on('click', changeMeasurement);
+
+  function addIcon(){
+    idMap = {
+      '2': "11d.png'>",
+      '3': "09d.png'>",
+      '5': "10d.png'>",
+      '6': "13d.png'>",
+      '7': "50d.png'>",
+      '8': "01d.png'>"
+    };
+    let html = "<img src='http://openweathermap.org/img/w/" + idMap[id.toString().charAt(0)];
+    $("#icon").html(html);
   }
   
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
-      $scope.latitude = position.coords.latitude;
-      $scope.longitude = position.coords.longitude;
-      weatherService.getWeather($scope.latitude, $scope.longitude, function(response){
-        $scope.weatherData = response.data;
-        console.log($scope.weatherData);
-        $scope.temp = parseInt($scope.weatherData.main.temp) + String.fromCharCode(176) + "C";
-        $scope.location = $scope.weatherData.name + ", " + $scope.weatherData.sys.country;
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      var url = 'https://fcc-weather-api.glitch.me/api/current?lat='+latitude+'&lon='+longitude;
+      $.getJSON(url, function(data){
+        weatherData = data;
+        temp = parseInt(weatherData.main.temp) + String.fromCharCode(176) + "C";
+        $('#temp').html(temp);
+        location = weatherData.name + ", " + weatherData.sys.country;
+        $('#location').html(location);
+        id = weatherData.weather[0].id;
+        addIcon();
       });
     });
   }
   
-})
+});
